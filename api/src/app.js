dc1e06a2e3e03b205e58bedde9475f645d722891
Ziad Nashaat <212103116@o6u.edu.eg>
2026-01-16 23:32:04 +0200

    feat: add basic Express server with health check endpoint
    - Created app.js to set up Express application
    - Added /health route to respond with a simple "ok" message
    - Implemented 404 error handling for undefined routes
    - Created server.js to initialize and start the server
    - Configured server to listen on a port defined in environment variables

---
+++ api/src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).send("ok");
+});
+
+app.use((req, res) => {
+  return res.status(404).json({
+    status: "fail",
+    message: `${req.originalUrl} not found!`,
+  });
+});
+
+export default app;
+// export {app};
