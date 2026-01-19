722d533d25b9dcca9f19ed38321861c6b10ae4b3
Ziad Nashaat <ziadnashaat03@gmail.com>
2026-01-19 07:57:17 +0200

    feat: add /test endpoint to return success message

--- api/src/app.js
+++ api/src/app.js
@@ -8,6 +8,14 @@ app.get("/health", (req, res) => {
   res.status(200).send("ok");
 });
 
+app.get('/test', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+        message: "Device successfully deleted",
+        data:{}
+  })
+})
+
 app.use((req, res) => {
   return res.status(404).json({
     status: "fail",
