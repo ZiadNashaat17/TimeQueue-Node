722fb9de03488463259e91644967795cda3c5040
Ziad Nashaat <ziadnashaat03@gmail.com>
2026-01-19 07:57:33 +0200

    feat: implement /test endpoint to fetch data from local server

--- scheduler/src/app.js
+++ scheduler/src/app.js
@@ -1,11 +1,19 @@
+import axios from "axios";
 import express from "express";
 
-const app = express();
 
+const app = express();
 app.use(express.json());
 
 app.get("/health", (req, res) => {
   res.status(200).send("ok");
+});
+
+app.get("/test", async (req, res) => {
+  const response = await axios.get("http://localhost:3000/test");
+  console.log(response);
+
+  res.status(response.status).json(response.message);
 });
 
 app.use((req, res) => {
